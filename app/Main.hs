63287286cf213920419f673162e57eac3fc70991
Josiah Berkebile <praenato14@gmail.com>
2024-11-05 15:12:55 -0600

    remove superfluous comment

--- app/Main.hs
+++ app/Main.hs
@@ -90,13 +90,6 @@ snapInstall channelName binName packageName foundPrefix foundErrText =
                   die ("ERROR: Could not install " <> packageName)
       Just loc -> echoWhichLocation loc foundPrefix foundErrText
 
--- echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
--- echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
--- curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
--- sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
--- sudo apt-get update
--- sudo apt-get install sbt
-
 installSbt :: IO ()
 installSbt = do
   sbtInstalled <- which "sbt"
